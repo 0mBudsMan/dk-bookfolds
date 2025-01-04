dbdca738116bd48d393c1a78df8085686266570c
0mBudsMan <buddhadevom@gmail.com>
2025-01-05 04:21:04 +0530

    static

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,38 +4,28 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 import {CgShoppingCart} from 'react-icons/cg'
 import { useStateContext } from '../../context/StateContext';
 
-const ProductDetails = ({products, product}) => {
-    const { image, name, details, price, tags, care } = product;
+
+
+const ProductDetails = ({ product }) => {
+    const { name, details, care, priceoriginal, pricediscounted, img } = product;
     const [index, setIndex] = useState(0);
-    const {decQty, incQty, qty, onAdd} = useStateContext();
+    const { decQty, incQty, qty, onAdd } = useStateContext();
 
-    const careList = [];
-
-    {for (let i = 0; i < care.length; i++) {
-        careList.push(care[i].children[0].text)
-    }}
+    // Path to the image
+    const imagePath = `/assets/${img}`;
 
     return (
         <div className='products'>
             <div className='product-detail-container'>
                 <div className='product-images'>
-                    <div className='small-images-container'>
-                        {image?.map((item, ind) => (
-                            <img 
-                            key={ind}
-                            src={urlFor(item)} 
-                            className='small-image' 
-                            onMouseEnter={() => setIndex(ind)} />
-                        ))}
-                    </div>
+                    {/* Render the image */}
                     <div className='big-image-container'>
-                        <img src={urlFor(image && image[index])} />
+                        <img src={imagePath} alt={name} className='big-image' />
                     </div>
                 </div>
                 <div className='product-details'>
                     <div className='name-and-category'>
                         <h3>{name}</h3>
-                        <span>{tags}</span>   
                     </div>
                     <div className='size'>
                         <p>SELECT SIZE</p>
@@ -51,13 +41,15 @@ const ProductDetails = ({products, product}) => {
                         <h4>Quantity: </h4>
                         <div>
                             <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
-                            <span className='num' onClick=''>{qty}</span>
+                            <span className='num'>{qty}</span>
                             <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
                         </div>
                     </div>
                     <div className='add-to-cart'>
-                        <button className='btn' type='button' onClick={() => onAdd(product, qty)}><CgShoppingCart size={20} />Add to Cart</button>
-                        <p className='price'>${price}.00</p>  
+                        <button className='btn' type='button' onClick={() => onAdd(product, qty)}>
+                            <CgShoppingCart size={20} />Add to Cart
+                        </button>
+                        <strike>${priceoriginal}</strike> ${pricediscounted}
                     </div>
                 </div>
             </div>
@@ -67,37 +59,42 @@ const ProductDetails = ({products, product}) => {
                     <div className="desc-background">
                         Overview
                     </div>
-                    <h2>Product Information</h2>  
+                    <h2>Product Information</h2>
                 </div>
                 <div className='desc-details'>
                     <h4>PRODUCT DETAILS</h4>
-                    <p>{details[0].children[0].text}</p>  
+                    <p>{details}</p>
                 </div>
-                <div className='desc-care'>
+                <div className='desc-c'>
                     <h4>PRODUCT CARE</h4>
                     <ul>
-                    {careList.map(list => (
-                        <li>{list}</li>
-                    ))}
+                        {care.map((list, index) => (
+                            <li key={index}>{list}</li>
+                        ))}
                     </ul>
                 </div>
             </div>
         </div>
-    )
-}
-export default ProductDetails
+    );
+};
 
-export const getStaticProps = async ({params: {slug}}) => {
-    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-    const productsQuery = '*[_type == "product"]'
-    const product = await client.fetch(query);
-    const products = await client.fetch(productsQuery)
-  
+export default ProductDetails;
+
+
+import path from 'path';
+import fs from 'fs/promises';
+
+export const getStaticProps = async () => {
+    const filePath = path.join(process.cwd(), 'public', 'data', 'dummy.json'); 
+    const jsonData = await fs.readFile(filePath, 'utf-8'); 
+    const data = JSON.parse(jsonData); 
+
     return {
-      props: { products, product }
-    }
-}
-
+        props: {
+            product: data[0] 
+        }
+    };
+};
 // Generates `/product/1` and `/product/2`
 export const getStaticPaths = async () => {
     const query = `*[_type == "product"] {
